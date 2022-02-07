4c758d453991e2b4f5fa310f0d6506e7759183a5
mextie <62155098+mextie-dev@users.noreply.github.com>
2022-02-07 13:03:48 -0600

    commit

---
+++ src/main/java/manyshields/manyshields/Manyshields.java
@@ -0,0 +1,10 @@
+package manyshields.manyshields;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Manyshields implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/manyshields/manyshields/client/ManyshieldsClient.java
@@ -0,0 +1,13 @@
+package manyshields.manyshields.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ManyshieldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
