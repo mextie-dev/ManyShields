d84498d8c6e5b771ddb7fedc4b9146ac012e0379
mextie <62155098+mextie-dev@users.noreply.github.com>
2022-02-08 10:51:56 -0600

    fully implemented wooden shield, including banner support and recipie (same as standard shield but uses any kind of log in place of iron, itemname is manyshields:wooden_shield)

--- src/main/java/manyshields/manyshields/Manyshields.java
+++ src/main/java/manyshields/manyshields/Manyshields.java
@@ -1,10 +1,12 @@
 package manyshields.manyshields;
 
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricBannerShieldItem;
 import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.tag.ItemTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,7 +14,11 @@ import net.minecraft.util.registry.Registry;
 public class Manyshields implements ModInitializer {
 
     // FabricShieldItem(settings.maxDamage(durability), cooldownTicks, enchantability, repairItem)
-    public static final Item WOODEN_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(50).group(ItemGroup.COMBAT), 3, 2, ItemTags.PLANKS);
+    public static final Item WOODEN_SHIELD = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(50).group(ItemGroup.COMBAT), 3, 2, ItemTags.PLANKS);
+    // Stone Shield, 113 durability, very high cooldown, 13, low enchantability, 4, uses cobblestone to repair
+    public static final Item STONE_SHIELD = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(113).group(ItemGroup.COMBAT), 13, 4, Items.COBBLESTONE);
+    // Copper Shield, 478 durability, moderate cooldown, 7, moderate enchantability, 6, uses copper ingot to repair
+    public static final Item COPPER_SHIELD = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(478).group(ItemGroup.COMBAT), 7, 6, Items.COPPER_INGOT);
 
     @Override
     public void onInitialize() {
